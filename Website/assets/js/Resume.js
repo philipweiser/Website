6e7998284e37a51ee9e5a5f313c63d57ffb9f5d9
philipweiser <philipkrw@gmail.com>
2015-01-13 16:03:10 -0500

    MVC Website initial backup

---
+++ Website/assets/js/Resume.js
@@ -0,0 +1,47 @@
+$('.event').hide();
+
+function y2015(){
+	if($('#2015').is(':hidden')){
+		$('#2015').slideDown(1500);
+		$('#2015d').slideDown(1500);		
+	} else {
+		$('#2015').hide(1500);
+		$('#2015d').hide(1500);	
+	}
+}
+function y2013(){
+	if($('#2013').is(':hidden')){
+		$('#2013').slideDown(1500);
+		$('#2013d').slideDown(1500);		
+	} else {
+		$('#2013').hide(1500);
+		$('#2013d').hide(1500);	
+	}
+}
+function y2012(){
+	if($('#2012').is(':hidden')){
+		$('#2012').slideDown(1500);
+		$('#2012d').slideDown(1500);		
+	} else {
+		$('#2012').hide(1500);
+		$('#2012d').hide(1500);	
+	}
+}
+function y2011(){
+	if($('#2011').is(':hidden')){
+		$('#2011').slideDown(1500);
+		$('#2011d').slideDown(1500);		
+	} else {
+		$('#2011').hide(1500);
+		$('#2011d').hide(1500);	
+	}
+}
+function y2006(){
+	if($('#2006').is(':hidden')){
+		$('#2006').slideDown(1500);
+		$('#2006d').slideDown(1500);		
+	} else {
+		$('#2006').hide(1500);
+		$('#2006d').hide(1500);	
+	}
+}
