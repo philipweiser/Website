170cc37b3d54ccf4e8872f84b3f86f381b831d80
philipweiser <philipkrw@gmail.com>
2015-02-23 10:29:18 -0500

    Lots of updates

---
+++ Website/obj/Release/Package/PackageTmp/assets/js/Posts_Index.js
@@ -0,0 +1,37 @@
+﻿$('#commentBtn').hide();
+function closeCommentBox(ele){
+    $(ele).attr("placeholder", "Reply");
+    $(ele).css("width", "50px");
+    $(ele).css("height", "15px");
+}
+function showComments(ele, buttonEle) {
+    $('#'+ele.id).slideDown(1000);
+    $('#' + buttonEle.id).attr("value", "Hide comments").attr("onclick", "hideComments(" + ele.id + ", " + buttonEle.id + ")");
+}
+function hideComments(ele, buttonEle) {
+    $('#'+ele.id).slideUp(500);
+    $('#' + buttonEle.id).val("Show comments...").attr("onclick", "showComments(" + ele.id + ", " + buttonEle.id + ")");
+}
+function commentBoxClicked(ele) {
+    $(ele).attr("placeholder", "");
+    $(ele).css("width", "100%");
+    $(ele).css("height", "120px");
+    $(ele).show();
+    $('#commentBtn').show();
+}
+function submitComment(Id) {
+    
+    var options = {
+        url: '/Comments/Create',
+        type: 'POST',
+        data: { PostId: Id, CommentBody: $('#submitBody').val()}
+    }
+    $.ajax(options).then(function (response){
+        $('#submitBody').val(response);
+        $('#commentBtn').hide();
+    },
+    function (error) {
+        console.log(error);
+        //output error in some way
+    });
+}
---
+++ Website/obj/Release/Package/PackageTmp/bin/Scripts/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+using System.IO;
+using System.Reflection;
+namespace ConsoleSln
+{
+    public class Program
+    {
+        [WebMethod]
+        public static void Main(string[] args)
+        {
+            int count = 0;
+            foreach (KeyValuePair<string, int> foo in wordFreq())
+             {
+                 if (foo.Value == count)
+                 {
+                     Console.Write(", {0}", foo.Key);
+                 }
+                 else if (foo.Value > count)
+                 {
+                     count = foo.Value;
+                 }
+                 else
+                 {
+                     Console.Write("\n\nCount = {0}: {1}", foo.Value, foo.Key);
+                     count = foo.Value;
+                 }
+             }            
+            Console.ReadKey();
+        }
+        [WebMethod]
+        public static int? maxOfThree(int[] numbers)
+        {
+            int? max = null;
+            foreach (var number in numbers)
+            {
+                if (max == null)
+                    max = number;
+                else if (max.Value < number)
+                    max = number;
+            }
+            return max.Value;
+        }
+        [WebMethod]
+        public static int[] sumMult(int[] numbers)
+        {
+            int theSum = 0;
+            foreach (int i in numbers)
+                theSum += i;
+
+            int answer = 1;
+            foreach (int i in numbers)
+                answer *= i;
+            return new int[2]{theSum, answer};
+        }
+        [WebMethod]
+        public static int factorial(int number)
+        {
+            int answer = 1;
+            for (int i = number; i > 0; i--)
+            {
+                answer *= i;
+            }
+            return answer;
+        }
+        [WebMethod]
+        public static bool palindrome(string foo)
+        {
+            for (int i = 0; i < foo.Length-1; i++)
+            {
+                if (foo[i] != foo[foo.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+                return true;
+        }
+        [WebMethod]
+        public static string[] fizzBuzz()
+        {
+            string[] answer = new string[100];
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    answer[i-1] = "FizzBuzz";
+                }
+                else if (i % 5 == 0)
+                {
+                    answer[i-1] = "Buzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    answer[i-1] = "Fizz";
+                }
+                else
+                {
+                    answer[i-1] = i+"";
+                }
+            }
+            return answer;
+        }
+        [WebMethod]
+        public static string longestWord()
+        {
+            string [] tokens;
+            string answer = "";
+            string line = new System.Net.WebClient().DownloadString("http://pweiser.azurewebsites.net/assets/alicechap1.txt");
+            tokens = line.Split(new char[] {' ', ',', '-', '"', '?', '.', '\n'});
+            foreach (string foo in tokens)
+            {
+                string temp = foo.Trim('\'');
+                if (temp.Length > answer.Length)
+                {
+                    answer = String.Copy(temp);
+                }
+            }
+            return answer;
+        }
+        [WebMethod]
+        public static List<string> filterLongWords(int length)
+        {
+            string[] tokens;
+            List<string> answer = new List<string>();
+            string line = new System.Net.WebClient().DownloadString("http://pweiser.azurewebsites.net/assets/alicechap1.txt");
+            tokens = line.Split(new char[] { ' ', ',', '-', '"', '?', '.' ,'!', ')', '(', '\'', ':', ';', '\n'});
+            foreach (string foo in tokens)
+            {
+                string temp = foo.Trim('\'');
+                if (temp.Length >= length)
+                {
+                    answer.Add(String.Copy(temp));
+                }
+            }
+            
+            return answer;
+        }
+        [WebMethod]
+        public static List<KeyValuePair<string, int>> wordFreq() {
+            string[] tokens;
+            Dictionary<string, int> answer = new Dictionary<string,int>();
+            string line = new System.Net.WebClient().DownloadString("http://pweiser.azurewebsites.net/assets/alicechap1.txt");
+            string extra ="";
+            extra = line.Replace(" \'", " ");
+            extra = extra.Replace("\' ", " ");
+            tokens = line.Split(new char[] { ' ', ',', '-', '"', '?', '.', '!', ')', '(', ':', ';', '\n'});
+            foreach (string foo in tokens)
+            {
+                string temp = foo.Trim('\'');
+                if (temp.Length > 0)
+                {
+                    if (answer.ContainsKey(temp))
+                    {
+                        answer[temp]++;
+                    }
+                    else
+                    {
+                        answer.Add(temp, 1);
+                    }
+                }
+
+            }
+            List<KeyValuePair<string, int>> myList = answer.ToList();
+
+            myList.Sort((firstPair, nextPair) =>
+            {
+                return firstPair.Value.CompareTo(nextPair.Value);
+            });
+            myList.Reverse(0, myList.Count);
+            return myList;
+        }
+        [WebMethod]
+        public static int countAlice()
+        {
+            int answer = 0;
+            string[] tokens;
+            string line = new System.Net.WebClient().DownloadString("http://pweiser.azurewebsites.net/assets/alicechap1.txt");
+            tokens = line.Split(new char[] { ' ', ',', '-', '"', '?', '.', '!', ')', '(', ':', ';', '\n' });
+            foreach (string foo in tokens)
+            {
+                if (foo.ToLower() == "alice")
+                {
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
